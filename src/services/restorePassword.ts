14f6abe94cea0baf9d0c1015739d9337866b4c23
bondiano <batbondik0@gmail.com>
2019-05-02 03:44:12 +0700

    Start make restore Password

---
+++ src/services/restorePassword.ts
@@ -0,0 +1,7 @@
+type RestorePasswordPayload = {
+  login: string
+}
+
+const restorePassword = request => (payload: RestorePasswordPayload) => new Promise(() => {})
+
+export default restorePassword
