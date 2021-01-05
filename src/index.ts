8aca95f34c39c035c853b7754e11963e9ca59cd2
Artem Zverev <artem.zverev@abbyy.com>
2021-01-05 23:41:50 +0700

    add services

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,25 @@
 import './scripts/init-partner'
 import request from './request'
+import { createJWTServices } from './jwt'
 import { createLoginService, createSignUpService } from './services'
+export * from './types'
 
 type CreateCasServices = {
   casURI: string
   partnerId: string
+  publicKey: string
 }
 
-export const createCasServices = ({ casURI, partnerId }: CreateCasServices) => {
+export const createCasServices = ({ casURI, partnerId, publicKey }: CreateCasServices) => {
   const baseRequest = request.create({ baseURL: casURI })
 
   const loginService = createLoginService(baseRequest)
 
   const signUpService = createSignUpService(baseRequest, partnerId)
 
-  return { loginService, signUpService }
+  const { verifyToken } = createJWTServices({ publicKey })
+
+  return { loginService, signUpService, verifyToken }
 }
 
 export default createCasServices
---
+++ src/types.ts
@@ -0,0 +1,37 @@
+export type User = {
+  activated: boolean
+  username: string
+  _id: string
+  email: string
+}
+
+export type SuccessAnswer<D> = {
+  success: true
+  data: D
+}
+
+export type ErrorAnswer<E extends string = ''> = {
+  success: false
+  error: {
+    errorMessage: E
+  }
+}
+
+export type SuccessLogin = SuccessAnswer<{ token: string; user: User }>
+
+export type ErrorLogin = ErrorAnswer<'user not found'>
+
+export type LoginCredentials = {
+  login: string
+  password: string
+}
+
+export type SignUpCredentials = {
+  username: string
+  password: string
+  email: string
+}
+
+export type SuccessSignUp = SuccessAnswer<User & { partnerId: string }>
+
+export type ErrorSignUp = ErrorAnswer
