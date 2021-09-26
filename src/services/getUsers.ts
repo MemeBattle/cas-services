83f16c68ca52f55a8e8ada26936ff739c5395816
Artem Zverev <themezv@gmail.com>
2021-09-26 21:23:35 +0300

    Fix typings of get users errors

--- src/services/getUsers.ts
+++ src/services/getUsers.ts
@@ -1,9 +1,9 @@
 import { AxiosInstance } from 'axios'
 import { CAS_ROUTES } from '../constants'
-import { SuccessGetUsers, GetUsersPayload } from '../types'
+import { SuccessGetUsers, GetUsersPayload, ErrorGetUsers } from '../types'
 
 export const createGetUsersService = (request: AxiosInstance) => (params?: GetUsersPayload) => {
-  return request.get<SuccessGetUsers, SuccessGetUsers>(`${CAS_ROUTES.users}`, {
+  return request.get<SuccessGetUsers | ErrorGetUsers, SuccessGetUsers | ErrorGetUsers>(`${CAS_ROUTES.users}`, {
     params,
   })
 }
